8a68217be415bfd1674b2c52d0c9633f80ceac94
Adeleke Hammed Lekan <lekhad19@gmail.com>
2023-07-16 17:47:05 +0100

    Added Google Login Authentication

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -6,6 +6,9 @@ import shareVideo from '../assets/share.mp4';
 import logo from '../assets/logowhite.png';
 
 const Login = () => {
+  const responseGoogle = (response) => {
+    
+  }
   return (
     <div className="flex justify-starts items-center flex-col h-screen">
         <div className='relative w-full h-full'>
@@ -22,6 +25,26 @@ const Login = () => {
               <div className="p-5">
                 <img src={logo} width="130px" alt="logo" />
               </div>
+              <div className="shadow-2xl">
+                <GoogleLogin 
+                  clientId=''
+                  render={(renderProps) => (
+                      <button
+                      type='button'
+                      className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
+                      >
+                        <FcGoogle className='mr-4' />
+                        Sign in with Google  
+                      </button>
+                  )}
+                  onSuccess={responseGoogle}
+                  onFailure={responseGoogle}
+                  cookiePolicy="single_host_origin"
+                />
+              </div>
+              
             </div>
         </div>
       </div>
